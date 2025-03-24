498067fa64ce919c9868a8721aa041fe0e0ea233
Lewap333 <pkusznierczuk99@gmail.com>
2025-03-24 18:18:22 +0100

    WCF self-hosting
    prosty serwer i klient WCF, bindingi, metadane, wyjątki info o zadaniach w info.txt

---
+++ WCF_LAB1_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ServiceModel;
+using KSR_WCF1;
+using WCF_LAB1_1.ServiceReference1;
+
+namespace WCF_LAB1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ChannelFactory<IZadanie1>(
+                new NetNamedPipeBinding(),
+                new EndpointAddress("net.pipe://localhost/ksr-wcf1-test"));
+
+            var channel = factory.CreateChannel();
+
+            Console.WriteLine(channel.Test("WCF zadanie 1"));
+
+            try
+            {
+                channel.RzucWyjatek(true);
+            }
+            catch (FaultException<KSR_WCF1.Wyjatek> e)
+            {
+                channel.OtoMagia(e.Detail.magia);
+            }
+            
+            ((IDisposable)channel).Dispose();
+
+            factory.Close();
+
+
+            var channel2 = new ServiceReference1.Zadanie7Client("NetNamedPipeBinding_IZadanie7");
+
+            try
+            {
+                channel2.RzucWyjatek7("test", 123);
+            }
+            catch (FaultException<Wyjatek7> e)
+            {
+                Console.WriteLine($"Wyjatek zadanie 7: {e.Detail.opis}, {e.Detail.a}, {e.Detail.b}");
+            }
+        }
+    }
+}
---
+++ WCF_LAB1_2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Runtime.Serialization;
+using KSR_WCF1;
+
+namespace WCF_LAB1_2
+{
+
+    [ServiceContract]
+    public interface IZadanie7
+    {
+        [OperationContract]
+        [FaultContract(typeof(Wyjatek7))]
+        void RzucWyjatek7(string a, int b);
+
+        
+    }
+
+    [DataContract]
+    public class Wyjatek7 
+    {
+        public Wyjatek7(string opis, string a, int b)
+        {
+            this.opis = opis;
+            this.a = a;
+            this.b = b;
+        }
+
+        [DataMember]
+        public string opis { get; set; }
+        [DataMember]
+        public string a { get; set; }
+        [DataMember]
+        public int b { get; set; }
+    }
+
+    public class Zadanie2 : IZadanie2, IZadanie7
+    {
+        public string Test(string arg)
+        {
+            return "Testowanie zadania: " + arg;
+        }
+
+        public void RzucWyjatek7(string a, int b)
+        {
+            var info = new Wyjatek7("Wyjatek zadanie7", a, b);
+            throw new FaultException<Wyjatek7>(info);
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new ServiceHost(typeof(Zadanie2));
+
+            var b = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
+            if (b == null) b = new ServiceMetadataBehavior();
+            host.Description.Behaviors.Add(b);
+
+            host.AddServiceEndpoint(
+                ServiceMetadataBehavior.MexContractName, 
+                MetadataExchangeBindings.CreateMexNamedPipeBinding(),
+                "net.pipe://localhost/metadane");
+
+            host.AddServiceEndpoint(
+                typeof(IZadanie2),
+                new NetNamedPipeBinding(),
+                "net.pipe://localhost/ksr-wcf1-zad2");
+
+            host.AddServiceEndpoint(
+                typeof(IZadanie2),
+                new NetTcpBinding(),
+                "net.tcp://127.0.0.1:55765");
+
+            host.AddServiceEndpoint(
+                typeof(IZadanie7),
+               new NetNamedPipeBinding(),
+                "net.pipe://localhost/ksr-wcf1-zad7");
+
+            host.Open();
+
+            Console.ReadKey();
+
+            host.Close();
+        }
+    }
+}
